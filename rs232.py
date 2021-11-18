371d8e40aa6d0600b43d3bb8bebd02ae33712194
kalepu <52924039+kalepu4@users.noreply.github.com>
2021-11-18 13:16:45 +0000

    kjb

--- rs232.py
+++ rs232.py
@@ -1,18 +1,15 @@
 import time
-
 import serial
-
-
-ser=serial.Serial(
-    port='/dev/ttyAMA0',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-
+ser = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
 )
+counter=0
 ser.flushInput()
 while 1:
-    data_string=ser.readline().decode()
-    print(data_string)
+        x=ser.readline()
+        print x
