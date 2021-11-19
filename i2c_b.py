4c965a7653048ea22b1291945a3e355ce50b7581
kalepu <52924039+kalepu4@users.noreply.github.com>
2021-11-19 18:03:48 +0000

    final corrections

--- i2c_b.py
+++ i2c_b.py
@@ -1,26 +1,37 @@
-# I2C with BM400
+# I2C with BM400 
 
-from re import X
+from re import X 
 import smbus
-import time
+import time 
 import math
+import urllib.request
 from time import sleep
 
-bus= smbus.SMBus(1)
+bus= smbus.SMBus (1)
 
-#switch from sleep to normal mode ACC_CONFIG0
-bus.write_byte_data(0x15,0x19,0x02) 
+urll='https://api.thingspeak.com/update?api_key=0S0P7Z4TPI9587KG&fieldl=0'
+a= ''
+#switch from sleep to normal mode ACC CONFIGO 14 bus.write_byte data (0x15,0x19,0x02)
+bus.write_byte_data(0x15,0x19,0x02)
 #leave some turn on time
-time.sleep(1.5)
-
-while 1: 
-    time.sleep(0.1)
-    #from the addr 0x15 and register 0x4, read 6 following bytes
-    data=bus.read_i2c_block_data(0x15,0x04,6)
-    #data is now in pairs of bytes of MSB and LSB for X Y and Z,
-    print(data)
-    x=data[0]+data[1]*256
-    y=data[2]+data[3]*256
-    c=y/x
-    angle= math.asin(c)
-    print(angle)
+time.sleep (1.5)
+print("Initializing I2C") 
+while 1:
+	print("Reading 12c") 
+	time.sleep(0.1)
+	#from the addr 0x15 and register 0x4 read 6 following bytes
+	data=bus.read_12c_block_data(0x15,0x04,6) 
+	#data is now In pairs of bytes of MSB and LSB for X Y and Z.
+	#print (data),
+	y=data[2]+data[3]*256
+	a=0.176*y
+	if a<360:
+		print(a)
+	else:
+		a=a-720 
+		print (a)
+	str_a=str(a)
+	uploadl=urllib.request.urlopen(urll+"&field5-0"+str_a)
+	upload1.read()
+	upload1.close()
+	counter=0
--- things.py
+++ things.py
@@ -1,40 +1,49 @@
 import time
+
 import serial
+
 from time import sleep
+
 import re
-import urllib.request as urlib2
 
-ser = serial.Serial(
-        port='/dev/ttyAMA0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
+import urllib.request
+
+from subprocess import call
+
+
+ser = serial.Serial(port='/dev/ttyAMA0', baudrate = 9600, parity-serial.PARITY NONE, stopbits-serial.STOPBITS_ONE, bytesize-serial.EIGHTBITS, timeout=1 }
+
+ldr='' 
+ctemp=''
+ftemp=""
+brake=''
+
 counter=0
+
 ser.flushInput()
-url1='https://api.thingspeak.com/update?api_key=OS0P7Z4TPI9587KG&field1=0'
+
+url1= https://api.thingspeak.com/update?api_key=0S0P7Z4TPI9587KG&fieldl=0'
+
 while 1:
-        data_string=ser.readline().decode()
-        data_float= re.findall('\d+(?:\.\d+)?',data_string)
-        NID=data_float[1]
 
-        if NID=="1":
-                ldr=data_float[2]
-        elif NID=="2":
-                ctemp=data_float[2]
-        elif NID=="3":
-                brake=data_float[2]
-        elif NID=="4":
-                ftemp=data_float[2]
+	data_string=ser.readline().decode() 
+	data_float= re.findall('\d+(?:\.\d+)?, data_string) 
+	NID=data_float [1]
 
-        counter=counter+1
-        print(data_float)
+	if NID=="1":
+		ldr=data_float [2] 
+	elif NID=="2":
+		ctemp=data_float [2] 
+	elif NID=="3":
+		brake=data float [2]
+	elif NID=="4":
+		ftemp=data_float [2]
 
-if counter>=4:
-    
-                upload1=urllib2.urlopen(url1+ldr+"&field2=0"+ctemp+"&field3=0"+brake+"&field4=0"+ftemp)
-                upload1.read()
-                upload1.close()
-                counter=0
+	counter=counter+1
+	print (data_float)
+
+	if counter>=4:
+		upload1=urllib.request.urlopen(urll+ldr+"&field2=0"+ctemp+"&field3=0"+brake+"&field4=0"+ftemp)
+		upload1.read()
+		upload1.close()
+		counter=0
